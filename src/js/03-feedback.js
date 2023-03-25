e71a8c45d6e9c103b68c506886e6fdc1000ae14d
Dana <dana.lalovaod@gmail.com>
2023-03-25 18:30:38 +0200

    fix: 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,12 +10,12 @@ refs.form.addEventListener('input', throttle(onInputHandler, 500));
 
 const LOCAL_KEY = "feedback-form-state";
 const formData = {};
-
+    
 
 // При перезагрузці сторінки витягую в інпути дані з local storage
 if (localStorage.getItem(LOCAL_KEY)){
     const parsedData =  JSON.parse(localStorage.getItem(LOCAL_KEY));
-    console.log(parsedData);
+    // console.log(parsedData);
     
     // В противном случае поля должны быть пустыми.
    if (parsedData.email){
@@ -39,9 +39,23 @@ refs.form.addEventListener('submit', onSubmitHandler);
 
 // При submit форми очищаю поля та сторедж
 function onSubmitHandler (event){
+    console.log(formData);
     event.preventDefault();
     event.currentTarget.reset();
     if (localStorage.getItem(LOCAL_KEY)){
         localStorage.removeItem(LOCAL_KEY);
     }
+}
+
+
+// ЗБЕРІГАЮ ДАНІ В formData
+if (refs.email.value){
+    console.log(refs.email.value);
+    formData.email = refs.email.value;
+}
+
+if (refs.message.value){
+     console.log(refs.message.value);
+
+    formData.message = refs.message.value;
 }
